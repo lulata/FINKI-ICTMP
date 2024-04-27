0e5f9122af31f3bac08698ccadf697fc53ab17f1
David Atanasoski <61380566+lulata@users.noreply.github.com>
2024-04-27 16:21:06 +0200

    shop page updates

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -1,81 +1,76 @@
 <template>
-  <div class="product-page">
-    <div class="product-page-header"></div>
-  </div>
-
-  <div class="container">
-    <h4>Route</h4>
-    <div class="productPage">
-      <div class="card flex-row">
-        <img class="card-img-left example-card-img-responsive" src="" />
-        <div class="card-body">
-          <h4 class="card-title h3">Product Title</h4>
-          <h6>Price $$$</h6>
-          <p class="card-text">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
-            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-          <div class="container">
-            <div class="row">
-              <span v-for="(size, index) in 5" :key="index" class="col-md-1">
-                <button type="button" class="btn btn-info">Info</button>
-              </span>
-            </div>
+  <div class="product-page" v-if="product">
+    <div class="product-page-header">
+      <div class="product-page-header-image">
+        <img :src="convertByteToImage(product.byteImage || '')" alt="" />
+      </div>
+      <div class="product-page-header-info">
+        <p class="product-page-header-info-name">{{ product.name }}</p>
+        <p class="product-page-header-info-price">{{ product.price }} Den</p>
+        <p class="product-page-header-info-description" v-if="product.description">{{ product.description }}</p>
+        <div class="product-page-header-info-sizes" v-if="product.sizes && product.sizes.length > 0">
+          <p class="product-page-header-info-sizes-title">Select size:</p>
+          <div class="product-page-header-info-sizes-box">
+            <button
+              class="product-page-header-info-sizes-box-size"
+              v-for="size in product.sizes"
+              :key="size.id"
+              :disabled="size.quantity === 0"
+              @click="selectSize(size.id as number)"
+              :class="{ 'product-page-header-info-sizes-box-size-selected': sizeSelected === size.id }">
+              {{ size.name }}
+            </button>
           </div>
-          <button type="button" class="btn btn-primary">Add To Cart</button>
         </div>
+        <button class="product-page-header-info-button">Add to cart</button>
       </div>
     </div>
-  </div>
-  <div class="productPage">
-    <h3>More About the product</h3>    
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
     <div class="best-seller-box">
       <p class="best-seller-box-title">Similar products</p>
       <div class="best-seller-box-items">
-        <div class="best-seller-box-items-box">
-          <router-link
-            v-for="product in products"
-            :key="product.name"
-            :to="{ name: 'ProductPage', params: { id: product.id } }">
-            <ItemCard :product="product" />
-          </router-link>
-        </div>
+        <router-link
+          v-for="product in products"
+          :key="product.name"
+          :to="{ name: 'ProductPage', params: { id: product.id } }">
+          <ItemCard :product="product" />
+        </router-link>
       </div>
     </div>
-</div>
-    
+  </div>
 </template>
 <script lang="ts" setup>
 import { Product } from '@/types';
 import ItemCard from '@/components/ItemCard.vue';
 import axios from 'axios';
 import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
 
 const products = ref<Product[]>([]);
+const product = ref<Product | null>(null);
+const route = useRoute();
+const sizeSelected = ref<number | null>(null);
 
 onMounted(() => {
-  getProductsForLandingPage();
+  Promise.all([getProduct(), getProducts()]);
 });
 
-function getProductsForLandingPage() {
+function getProduct() {
+  axios.get(`/api/product/${route.params.id}`).then((response) => {
+    product.value = response.data;
+  });
+}
+
+function getProducts() {
   axios.get('/api/product/all').then((response) => {
     products.value = response.data.slice(0, 4);
   });
 }
+
+function convertByteToImage(byteImage: string) {
+  return `data:image/png;base64,${byteImage}`;
+}
+
+function selectSize(sizeId: number) {
+  sizeSelected.value = sizeId;
+}
 </script>
-<style lang="scss" scoped>
-h4 {
-  padding-top: 20px;
-}
-h3 {
-  padding-top: 20px;
-}
-.col-md-1 {
-  margin-left: 130px;
-}
-</style>
