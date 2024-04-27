c339dfae91e3e62f42e8a91853c288f5f6c7d66d
David Atanasoski <61380566+lulata@users.noreply.github.com>
2024-04-27 15:04:53 +0200

    updates to category page

--- src/views/CategoryPage.vue
+++ src/views/CategoryPage.vue
@@ -8,61 +8,73 @@
       </div>
     </div>
     <div class="category-page-body">
+      <div class="category-page-body-categories">
+        <div
+          class="category-page-body-categories-category"
+          v-for="category in categories"
+          :key="category.id"
+          :class="{
+              'category-page-body-categories-category-selected': selectedCategories.includes(category.id as number),
+            }"
+          @click="selectCategory(category)">
+          {{ category.name }}
+        </div>
+      </div>
       <div class="category-page-body-items">
         <router-link
           :to="{ name: 'ProductPage', params: { id: product.id } }"
-          v-for="product in products"
+          v-for="product in filteredProducts"
           :key="product.id">
           <ItemCard :product="product" />
         </router-link>
       </div>
     </div>
   </div>
-
-  <div class="container">
-    <div class="row">
-      <span v-for="(size, index) in 6" :key="index" class="col-md-1">
-        <button type="button" class="btn btn-info">Info</button>
-      </span>
-    </div>
-  </div>
 </template>
 <script lang="ts" setup>
 import ItemCard from '@/components/ItemCard.vue';
-import { Product } from '@/types';
+import { Category, Product } from '@/types';
 import axios from 'axios';
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
 
 const products = ref<Product[]>([]);
+const categories = ref<Category[]>([]);
+const selectedCategories = ref<number[]>([]);
 
-onMounted(() => {
-  Promise.all([getProducts()]);
+const filteredProducts = computed(() => {
+  const category = router.currentRoute.value.params.id === 'men' ? 'MALE' : 'FEMALE';
+  const filteredBySex = products.value.filter(
+    (product) => product.gender === category || product.gender.toLowerCase() === 'unisex'
+  );
+  return selectedCategories.value.length === 0
+    ? filteredBySex
+    : filteredBySex.filter((product) => selectedCategories.value.includes(product.categoryId));
 });
 
-let categories: any = ref([]);
+onMounted(() => {
+  Promise.all([getProducts(), getCategories()]);
+});
+
 function getCategories() {
-  axios.get('/api/admin/category/all').then((response) => {
+  return axios.get('/api/admin/category/all').then((response) => {
     categories.value = response.data;
   });
 }
 
 function getProducts() {
-  axios.get('/api/product/all').then((response) => {
+  return axios.get('/api/product/all').then((response) => {
     products.value = response.data;
   });
 }
-getCategories();
-console.log(categories);
-console.log(categories.value);
+
+function selectCategory(category: Category) {
+  if (selectedCategories.value.includes(category.id as number)) {
+    selectedCategories.value = selectedCategories.value.filter((c) => c !== category.id);
+  } else {
+    selectedCategories.value = [...selectedCategories.value, category.id as number];
+  }
+}
 </script>
-<style lang="scss" scoped>
-h1 {
-  text-align: center;
-}
-#mencontainer {
-  text-align: center;
-}
-</style>
